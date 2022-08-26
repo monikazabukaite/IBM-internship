432d0642aa72faed9ba3ab6de7953b08966e6ea9
monikazabukaite <monika.zabukaite@gmail.com>
2022-08-26 20:49:45 +0300

    Make company card responsive

--- internship-ibm/frontend/src/components/company-details/company-details.tsx
+++ internship-ibm/frontend/src/components/company-details/company-details.tsx
@@ -1,15 +1,20 @@
-import { Box } from "@mui/material";
+import { Box, Theme } from "@mui/material";
 import { useSelector } from "react-redux";
 import { selectCompanyData, selectLoading } from "../../stores/selectors";
 import { CompanyCard } from "./company-card";
 import Skeleton from "@mui/material/Skeleton";
 import { NotFound } from "../not-found";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 interface ICompanyDetailsProps {}
 
 export const CompanyDetails = (props: ICompanyDetailsProps) => {
   const companyData = useSelector(selectCompanyData);
   const isLoading = useSelector(selectLoading);
+
+  const matchesBreakpoint = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.up("md")
+  );
 
   if (isLoading) {
     return (
@@ -28,7 +33,9 @@ export const CompanyDetails = (props: ICompanyDetailsProps) => {
   return (
     <>
       {companyData && (
-        <Box sx={{ minWidth: "350px", width: "75%" }}>
+        <Box
+          sx={{ minWidth: "350px", width: matchesBreakpoint ? "75%" : "100%" }}
+        >
           <CompanyCard companyDetails={companyData} />
         </Box>
       )}
